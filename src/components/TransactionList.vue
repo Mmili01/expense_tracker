a716c632370e1f25e8f753d9c0525297cace90c2
Abba Emmanuel <eabba21@gmail.com>
2024-08-06 22:56:54 +0100

    added the delete feature

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -7,17 +7,25 @@
       :class="transaction.amount < 0 ? 'minus' : 'plus'"
     >
       {{ transaction.text }} <span>${{ transaction.amount }}</span
-      ><button class="delete-btn">X</button>
+      ><button @click="deleteTransaction(transaction.id)" class="delete-btn">
+        X
+      </button>
     </li>
   </ul>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
+
+const emit = defineEmits(["transactionDeleted"]);
 const props = defineProps({
   transactions: {
     type: Array,
     required: true,
   },
 });
+
+const deleteTransaction = (id) => {
+  emit("transactionDeleted", id);
+};
 </script>
